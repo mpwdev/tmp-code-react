1d4974d07a0f75da8a731bceb68e53af59f08e65
MP Web Developer <mprwdev@gmail.com>
2025-10-18 18:25:56 +0300

    refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
 
 import { CORE_CONCEPTS } from './data.js';
 
@@ -24,16 +23,6 @@ function Header() {
   );
 }
 
-// function CoreConcept(props) {
-//   return (
-//     <li>
-//       <img src={props.image} alt={props.title} />
-//       <h3>{props.title}</h3>
-//       <p>{props.description}</p>
-//     </li>
-//   );
-// }
-
 function CoreConcept({ image, title, description }) {
   return (
     <li>
@@ -53,55 +42,17 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept
-              title="title here"
-              description="description text"
-              image={componentsImg}
-            ></CoreConcept>
-            <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             ></CoreConcept>
-            <CoreConcept
-              title={CORE_CONCEPTS[1].title}
-              description={CORE_CONCEPTS[1].description}
-              image={CORE_CONCEPTS[1].image}
-            ></CoreConcept>
+            <CoreConcept {...CORE_CONCEPTS[1]}></CoreConcept>
+            <CoreConcept {...CORE_CONCEPTS[2]}></CoreConcept>
             <CoreConcept {...CORE_CONCEPTS[3]}></CoreConcept>
-          </ul>
-          <ul>
-            <TestCoreConcept
-              title="test title"
-              description="test description"
-              image="test-image.png"
-            ></TestCoreConcept>
-          </ul>
-          <ul>
-            <TestCoreConcept2 concept={CORE_CONCEPTS[0]}></TestCoreConcept2>
           </ul>
         </section>
       </main>
     </div>
-  );
-}
-
-function TestCoreConcept({ title, description, image }) {
-  return (
-    <li>
-      <p>{title}</p>
-      <p>{description}</p>
-      <p>{image}</p>
-    </li>
-  );
-}
-
-function TestCoreConcept2({ concept }) {
-  return (
-    <li>
-      <p>{concept.title}</p>
-      <p>{concept.description}</p>
-      <p>{concept.image}</p>
-    </li>
   );
 }
 

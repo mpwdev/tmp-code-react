dbd092b1b4c6bb206b1daa361eec3a6128c92582
MP Web Developer <mprwdev@gmail.com>
2025-10-18 18:57:17 +0300

    handle click event for button

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,11 @@
 export default function TabButton({ children }) {
+  function handleClick() {
+    console.log('clicked');
+  }
+
   return (
     <li>
-      <button>{children}</button>
+      <button onClick={handleClick}>{children}</button>
     </li>
   );
 }
